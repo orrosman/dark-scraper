66ad4f96d72fddbb85b3b3c14044b04739e51d0e
Or Rosman <42673555+orrosman@users.noreply.github.com>
2022-02-09 17:06:42 +0200

    basic page

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<nav className="navbar navbar-dark bg-dark">
+			<i className="navbar-brand mx-auto text-center">
+				<div>Scraper Dashboard</div>
+			</i>
+		</nav>
+	);
+};
+
+export default Header;
