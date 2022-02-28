d260d919a6fe4f15120d5458d3797d485427c042
Or Rosman <42673555+orrosman@users.noreply.github.com>
2022-02-28 12:22:22 +0200

    send data on first time initializing connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { addToDB, getPosts } from './utils/db';
-import { Post } from './models/post';
+import { addToDB } from './utils/db';
 import { scraper } from './utils/scraper';
+import { writeNewPosts } from './utils/network';
 
 const app: Application = express();
 const port = 8080;
@@ -22,15 +22,18 @@ setInterval(async () => {
 	}
 }, time_interval);
 
-app.get('/init', async (req: Request, res: Response) => {
+app.get('/init', async (_req: Request, res: Response) => {
 	res.writeHead(200, {
 		'Content-Type': 'text/event-stream',
 		'Cache-Control': 'no-cache',
 		Connection: 'keep-alive',
 	});
+
+	//initialize first data source
+	await writeNewPosts(res);
+
 	setInterval(async () => {
-		const newPosts: Post[] = await getPosts();
-		res.write(`data: ${JSON.stringify(newPosts)}\n\n`);
+		await writeNewPosts(res);
 	}, time_interval);
 });
 
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,4 +1,7 @@
+import { Response } from 'express';
 import axios from 'axios';
+import { getPosts } from '../utils/db';
+import { Post } from '../models/post';
 
 const url =
 	'http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all';
@@ -11,4 +14,9 @@ export const getSiteData = async () => {
 		},
 	});
 	return res.data;
+};
+
+export const writeNewPosts = async (res: Response) => {
+	const newPosts: Post[] = await getPosts();
+	res.write(`data: ${JSON.stringify(newPosts)}\n\n`);
 };
